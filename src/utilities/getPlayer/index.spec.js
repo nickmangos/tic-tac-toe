6b61a129b5ef4397877c5f221123bf8b729f525b
Nick Mangos <nick.mangos@gmail.com>
2018-10-21 14:19:04 +1300

    Add the get Player utility function

---
+++ src/utilities/getPlayer/index.spec.js
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import getPlayer from '.'
+
+describe('utilities:getPlayer', () => {
+  it('returns undefined if moves array not provided', () => {
+    expect(getPlayer(4)).toBeUndefined()
+  })
+
+  it('returns `x` for even-numbered moves', () => {
+    expect(getPlayer(4, [4, 0])).toBe('x')
+  })
+
+  it('returns `o` for odd-numbered moves', () => {
+    expect(getPlayer(0, [4, 0])).toBe('o')
+  })
+
+  it('returns undefined for empty squares (not moved yet)', () => {
+    expect(getPlayer(3, [4, 0])).toBeUndefined()
+  })
+})
