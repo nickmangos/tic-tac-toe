4e43b99b617cbfada83f866a7241f9a86cfe0a9a
Nick Mangos <nick.mangos@gmail.com>
2018-10-25 17:05:56 +1300

    Add the store

---
+++ src/state/store/index.spec.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import configureStore from '.'
+
+describe('state:store', () => {
+  describe('configureStore', () => {
+    it('creates a store', () => {
+      expect(configureStore()).toHaveProperty('getState')
+      expect(configureStore()).toHaveProperty('dispatch')
+      expect(configureStore()).toHaveProperty('subscribe')
+    })
+  })
+})
