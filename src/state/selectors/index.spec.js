0ac6a743953e94f3df99f16cdc42444f9df9fdc0
Nick Mangos <nick.mangos@gmail.com>
2018-10-25 17:03:37 +1300

    Add getMoves selector

---
+++ src/state/selectors/index.spec.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { getMoves } from '.'
+
+describe('state:selectors', () => {
+  describe('getMoves', () => {
+    it('extracts the moves array from the state', () => {
+      const moves = [4, 0, 2]
+      const state = { moves }
+
+      expect(getMoves(state)).toBe(moves)
+    })
+  })
+})
