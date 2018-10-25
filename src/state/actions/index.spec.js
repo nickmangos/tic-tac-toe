a51b43c5a8dc5a51b8882a5015ea341b9f58ecb1
Nick Mangos <nick.mangos@gmail.com>
2018-10-25 16:50:39 +1300

    Add squareClicked action creator

---
+++ src/state/actions/index.spec.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { squareClicked } from '.'
+import { SQUARE_CLICKED } from '..'
+
+describe('state:actions', () => {
+  describe('squareClicked', () => {
+    it('produces the correct action for clicking a Square', () => {
+      const square = 4
+
+      expect(squareClicked(square)).toMatchObject({
+        type: SQUARE_CLICKED,
+        payload: {
+          square: 4
+        }
+      })
+    })
+  })
+})
